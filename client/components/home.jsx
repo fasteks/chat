d1e630a2dda5a2e6369f8a70605a7576c12f8e53
Ivan <xpynkostb@gmail.com>
2022-04-14 11:49:15 +0300

    initial components

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Head from './head'
-// import wave from '../assets/images/wave.jpg'
 
 const Home = () => {
-  const [counter, setCounterNew] = useState(0)
 
   return (
-    <div>
-      <Head title="Dashboard" />
-      <img alt="wave" src="images/wave.jpg" />
-      <button type="button" onClick={() => setCounterNew(counter + 1)}>
-        updateCounter
-      </button>
-      <div> Hello World Dashboard {counter} </div>
+    <div className="min-h-screen">
+      <Head title="Home" />
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="">Welcome</p>
+      </div>
     </div>
   )
 }
