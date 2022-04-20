2a0b4b378031678e8abb92862d230284698e7105
fasteks <xpynkostb@gmail.com>
2022-04-20 17:45:21 +0300

    choose local or remote db path

--- server/config.js
+++ server/config.js
@@ -6,6 +6,8 @@ const options = {
   env: process.env.NODE_ENV,
   isSocketsEnabled: process.env.ENABLE_SOCKETS,
   mongoURL: process.env.MONGO_URL
+  // MONGO_URL=mongodb://127.0.0.1:27017
+  // MONGO_URL=mongodb+srv://<login>:<password>@cluster0.tqzaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 }
 
 export default options
